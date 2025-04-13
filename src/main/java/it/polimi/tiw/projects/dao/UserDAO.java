2aa25c5f7fe73fa7768b3d6df0aa187e2cfbd1fe
biscotto-home <zining.chen@mail.polimi.it>
2025-04-14 01:42:53 +0200

    added DAOexceptions and more functions

--- src/main/java/it/polimi/tiw/projects/dao/UserDAO.java
+++ src/main/java/it/polimi/tiw/projects/dao/UserDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import it.polimi.tiw.projects.beans.User;
+import it.polimi.tiw.projects.exceptions.DAOException;
 
 public class UserDAO {
 	private Connection connection;
@@ -14,24 +15,59 @@ public class UserDAO {
 		this.connection = connection;
 	}
 
-	public User checkCredentials(String username, String pwd) throws SQLException {
+	public void createUser(String username, String pwd, String name, String surname) throws SQLException, DAOException {
+		String insertQuery = "INSERT INTO user (username, password, name, surname) VALUES (?, ?, ?, ?)";
+		String checkExistence = "SELECT * FROM user WHERE username = ?";
+		try (PreparedStatement checkStatement = connection.prepareStatement(checkExistence);
+				PreparedStatement insertStatement = connection.prepareStatement(insertQuery);) {
+			checkStatement.setString(1, username);
+			ResultSet result = checkStatement.executeQuery();
+
+			if (result.next())
+				throw new DAOException("Username already exists", DAOException.DAOErrorType.USERNAME_ALREADY_EXISTS);
+			else {
+				insertStatement.setString(1, username);
+				insertStatement.setString(2, pwd);
+				insertStatement.setString(3, name);
+				insertStatement.setString(4, surname);
+				insertStatement.executeUpdate();
+			}
+		}
+	}
+
+	public User checkCredentials(String username, String pwd) throws SQLException, DAOException {
 		String query = "SELECT  idUser, username, name, surname FROM user  WHERE username = ? AND password =?";
 		try (PreparedStatement pStatement = connection.prepareStatement(query);) { // Sanitized
 			pStatement.setString(1, username);
 			pStatement.setString(2, pwd);
 			try (ResultSet result = pStatement.executeQuery();) {
-				if (!result.isBeforeFirst()) // no results, credential check failed
-					return null;
-				else {
-					result.next();
+				if (result.next()) { // A user was matched with the password
 					User user = new User();
 					user.setIdUser(result.getInt("idUser"));
 					user.setUsername(result.getString("username"));
 					user.setName(result.getString("name"));
 					user.setSurname(result.getString("surname"));
 					return user;
-				}
+				} else
+					throw new DAOException("Invalid credentials", DAOException.DAOErrorType.INVALID_CREDENTIALS);
+
 			}
+		}
+	}
+
+	public void modifyUser(User user, String name, String surname) throws SQLException, DAOException {
+		String query = "UPDATE user SET name = ?, surname = ? WHERE idUser = ?";
+
+		if (name == null)
+			name = user.getName();
+		if (surname == null)
+			surname = user.getSurname();
+
+		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
+			pStatement.setString(1, name);
+			pStatement.setString(2, surname);
+			pStatement.setInt(3, user.getIdUser());
+			pStatement.executeUpdate();
 		}
 	}
 }
---
+++ src/main/java/it/polimi/tiw/projects/exceptions/DAOException.java
@@ -0,0 +1,26 @@
+package it.polimi.tiw.projects.exceptions;
+
+public class DAOException extends Exception {
+
+    public enum DAOErrorType {
+        USERNAME_ALREADY_EXISTS,
+        INVALID_CREDENTIALS,
+        GENERIC_ERROR // For other potential DAO errors
+    }
+
+    private final DAOErrorType errorType;
+
+    public DAOException(String message, DAOErrorType errorType) {
+        super(message);
+        this.errorType = errorType;
+    }
+
+    public DAOException(String message, Throwable cause, DAOErrorType errorType) {
+        super(message, cause);
+        this.errorType = errorType;
+    }
+
+    public DAOErrorType getErrorType() {
+        return errorType;
+    }
+}
