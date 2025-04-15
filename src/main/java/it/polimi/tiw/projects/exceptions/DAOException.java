215ae92b61235e5d153a6eb47fcda71574592bb0
biscotto-home <zining.chen@mail.polimi.it>
2025-04-16 01:48:33 +0200

    Added a DAO error type

--- src/main/java/it/polimi/tiw/projects/exceptions/DAOException.java
+++ src/main/java/it/polimi/tiw/projects/exceptions/DAOException.java
@@ -5,7 +5,7 @@ public class DAOException extends Exception {
     private static final long serialVersionUID = 1L;
 
     public enum DAOErrorType {
-        USERNAME_ALREADY_EXISTS,
+        NAME_ALREADY_EXISTS,
         INVALID_CREDENTIALS,
         NOT_FOUND,
         GENERIC_ERROR // For other potential DAO errors
