00b681c987f0cd4d6e56739f98b87cb1ae05e78e
EdoardoBergamo <edoardo.bergamo@mail.polimi.it>
2025-04-23 00:30:24 +0200

    E: minor fixes

--- src/main/java/it/polimi/tiw/projects/controllers/GoToHome.java
+++ src/main/java/it/polimi/tiw/projects/controllers/GoToHome.java
@@ -1,16 +1,82 @@
 package it.polimi.tiw.projects.controllers;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import it.polimi.tiw.projects.beans.User;
+import it.polimi.tiw.projects.dao.PlaylistDAO;
+import it.polimi.tiw.projects.exceptions.DAOException;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.UnavailableException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class GoToHome extends HttpServlet {
-
-	public void init() {
-
+ static final long serialVersionUID = 1L;
+	private Connection connection;
+	private TemplateEngine templateEngine;
+	
+	public void init() throws ServletException {
+		
+		try {
+		ServletContext context = getServletContext();
+		String driver = context.getInitParameter("dbDriver");
+		String user = context.getInitParameter("dbUser");
+		String password = context.getInitParameter("dbPassword");
+		String url = context.getInitParameter("dbUrl");
+		Class.forName(driver);
+		
+		connection = DriverManager.getConnection(url, user, password);
+		
+		JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(context);
+		
+		WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(webApplication);
+		
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		
+		templateResolver.setSuffix(".html");
+		
+		templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new UnavailableException("Couldn't get db connection");
+		} catch (ClassNotFoundException e) {
+			throw new UnavailableException("Can't load database driver");
+		}
 	}
-
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		PlaylistDAO playlistDAO = new PlaylistDAO(connection);
+		
+		UUID userId = ((User) req.getSession().getAttribute("user")).getIdUser();
+		
+		try {
+			List<Integer> playlistIDs = playlistDAO.findPlaylistsByUser(userId);
+		} catch (DAOException e) {
+			e.printStackTrace();
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to sign up");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 }
