301b2d49d808f045e21fc5f121a55d02da4342e9
EdoardoBergamo <edoardo.bergamo@mail.polimi.it>
2025-05-26 18:14:53 +0200

    E: created mokup for playlist viewing page

---
+++ src/main/webapp/js/views/playlistView.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+export function renderPlaylistView(appContainer, playlist){
+	appContainer.innerHTML = '';
+	appContainer.style.maxWidth = '100%';
+	
+	const songGrid = document.createElement('div');
+	songGrid.className = 'grid fiveSong-grid';
+	
+	
+	
+	
+	
+	
+}
